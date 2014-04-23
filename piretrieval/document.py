6c8e069b5c42bf43fa5001e3a522c3f27b937f9b
Patrick Beedgen <patrick.beedgen@googlemail.com>
2014-04-23 00:57:56 +0000

    first code skeleton for ir processor

--- piretrieval/document.py
+++ piretrieval/document.py
@@ -1,4 +1,4 @@
 class Document(object):
 
-  def __init__(self):
-    self.vector = dict()
+    def __init__(self):
+        self.vector = dict()
---
+++ piretrieval/processor.py
@@ -0,0 +1,19 @@
+import document
+
+class Processor(object):
+
+    def __init__(self):
+        #words dictionary for lemmatizing
+        self.words = None
+        self.stopwords = None
+                
+    def lemmatize(word):
+        result = ""
+        return result
+
+    def load_lemmas(file_path):
+        #load lemmas from file
+        pass
+
+    def string_to_document(string):
+        pass
